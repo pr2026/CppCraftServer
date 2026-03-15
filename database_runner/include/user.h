b97f9858b4f58f02cecf841615fb1349a1472dac
Veronica Ruazanskaya <nicaryazanskaya@yandex.ru>
2026-03-15 23:15:45 +0300

    add to Database baseclass and UserDB

--- database_runner/include/user.h
+++ database_runner/include/user.h
@@ -6,7 +6,11 @@ struct User {
     std::string username;
     std::string password;
     std::string role;
-    User() : id(0) {}
-    User(int id, const std::string& username, const std::string& role): id(id), username(username), role(role),  {}
-};
 
+    User() : id(0) {
+    }
+
+    User(int id, const std::string &username, const std::string &role)
+        : id(id), username(username), role(role), {
+    }
+};
