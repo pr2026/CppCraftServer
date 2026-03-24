e9a87157313fbaa8681868ac0cdadcd14b46e49a
Yaroslavna <yaroslavna0808@gmail.com>
2026-03-24 21:31:03 +0300

    added a storage system for receiving solutions and sending them to the runner

---
+++ storage/MemorySubmissionStorage.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "SubmissionStorage.h"
+#include <unordered_map>
+#inlcude <mutex>
+
+class MemorySubmissionStorage : public SubmissionStorage {
+public:
+    MemorySubmissionStorage();
+    int addSubmission(int userId, int taskId, const std::string& code) override;
+    std::optional<Submission> getSubmissionById(int id) override;
+    bool updateSubmissionResult(int id, const SubmissionResult& result) override;
+
+private:
+    std::unordered_map<int, Submission> submissions;
+    std::mutex m;
+    int nextId = 1;
+};
