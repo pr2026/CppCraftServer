4b9ae6ffc1e61b1e1f70c5cb8d4bcec3f076e0f0
Veronica Ruazanskaya <nicaryazanskaya@yandex.ru>
2026-05-31 23:38:56 +0300

    merge

--- database_runner/include/SolutionDB.h
+++ database_runner/include/SolutionDB.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <string>
 #include "RunResult.h"
-<<<<<<< HEAD
 #include "Database.h"
 #include <vector>
 #include <sqlite3.h>
 #include <string>
-=======
->>>>>>> 8d73483877d337adbc4c45cb4dafcffde3789106
 
 struct Solution {
     int id;
@@ -15,10 +12,7 @@ struct Solution {
     int task_id;
     std::string code_path;
     Run_result result;
-<<<<<<< HEAD
     std::string created_at;
-=======
->>>>>>> 8d73483877d337adbc4c45cb4dafcffde3789106
 
     Solution() : id(0), user_id(0), task_id(0) {
     }
@@ -37,10 +31,3 @@ public:
     std::vector<Solution> getSolutionsForUser(int user_id);
     std::vector<Solution> getSolutionsForTask(int task_id);
 };
-=======
-    using Database::Database; 
-    bool addSolution(int user_id, int task_id, const std::string& code_path, const Run_result& result);
-    std::vector<Solution> getSolutionsForUser(int user_id);
-    std::vector<Solution> getSolutionsForTask(int task_id);
-}
->>>>>>> 8d73483877d337adbc4c45cb4dafcffde3789106
